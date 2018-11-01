474c8b3ff8ba03fdb30ced336937a755e4bac676
iris9112 <iris9112@gmail.com>
2018-11-01 02:10:34 -0500

    preparando django project

--- site_prediction/app/utils.py
+++ site_prediction/app/utils.py
@@ -3,11 +3,17 @@ import pandas as pd
 from .models import Document 
 
 def previewDataFrame():
+    """
+    preview a dataframe in a template
+    """
     d = Document.objects.all().last()
     # creando el data frame
     df = pd.read_csv(d.document.path)
 
     columns = ['User_ID', 'Product_ID', 'Gender', 'Age', 'Occupation', 'City_Category',
-               'Stay_In_Current_City_Years', 'Marital_Status', 'Product_Category_1',
-               'Product_Category_2', 'Product_Category_3', 'Purchase']
+               'Stay_In_Current_City_Years', 'Marital_Status', 'Product_Category_1', 'Purchase']
     return df.head().to_html()
+
+# ----------------------------------------------------------
+# prepare and organize the data
+
